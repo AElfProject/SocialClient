b8158389bc153da6cb63ef97198e50ac74662d31
Loning <aloning@gmail.com>
2018-04-03 13:21:06 +0800

    ci

---
+++ SocialClient.Tests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using LinqToTwitter;
+using Xunit;
+
+namespace SocialClient.Tests
+{
+    
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            var auth = new SingleUserAuthorizer
+            {
+                CredentialStore = new SingleUserInMemoryCredentialStore
+                {
+                    ConsumerKey = Environment.GetEnvironmentVariable(OAuthKeys.TwitterConsumerKey),
+                    ConsumerSecret = Environment.GetEnvironmentVariable(OAuthKeys.TwitterConsumerSecret),
+                    AccessToken = Environment.GetEnvironmentVariable(OAuthKeys.TwitterAccessToken),
+                    AccessTokenSecret = Environment.GetEnvironmentVariable(OAuthKeys.TwitterAccessTokenSecret)
+                }
+            };
+            var context = new TwitterContext(auth);
+        }
+    }
+}
